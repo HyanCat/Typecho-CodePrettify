8093fd3ad9c433583c93024ed43224e7e65bc58e
HyanCat <hyancat@live.cn>
2015-09-02 11:14:54 +0800

    make the right selector.

--- js/code.js
+++ js/code.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
-    $("code").addClass("prettyprint linenums");
+	// 只对代码块进行渲染，嵌入式 code tag 不渲染
+	// 没有指定语言不显示行号，指定语言才显示行号
+	$("pre code").addClass("prettyprint");
+    $("pre code[class*=lang-]").addClass("linenums");
     prettyPrint();
 });
